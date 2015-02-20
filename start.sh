dd881e27958a7b61f78da0a3669a688806afd712
Diego Pérez <eazel7@gmail.com>
2015-02-20 15:25:54 -0300

    -l 0.0.0.0

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+# IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
-(cd /c9sdk && node server -l $IP -w /workspace -p 80)
+# (cd /c9sdk && node server -l $IP -w /workspace -p 80 --collab)
+(cd /c9sdk && node server -l 0.0.0.0 -w /workspace -p 80)
